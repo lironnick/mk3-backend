6e56912e7f7c4a1649ab38cd81d7bb7712d957b0
ABC da contrucao <digital.abcdaconstrucao@gmail.com>
2020-03-26 16:15:14 -0300

    ajuste no pages para page index

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -3,14 +3,14 @@ const connection = require('../database/connections');
 module.exports = {
 
     async index(req, res) {
-        const {pages = 1 } = req.query;
+        const {page = 1 } = req.query;
 
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
-            .offset((pages -1) * 5)
+            .offset((page -1) * 5)
             .select([
                 'incidents.*', 
                 'ongs.name', 
